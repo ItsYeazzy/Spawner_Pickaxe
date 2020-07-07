a9452b5826f5161ed6861c5ea785b6831751bba4
royaljacquesytb <63215754+royaljacques@users.noreply.github.com>
2020-07-07 13:33:03 +0200

    test de code

--- src/royal/Main.php
+++ src/royal/Main.php
@@ -9,6 +9,9 @@ use pocketmine\Server;
 use pocketmine\item\Item;
 use pocketmine\item\ItemFactory;
 use pocketmine\event\Listener;
+use pocketmine\plugin\PluginManager;
+
+use onebone\EconomieAPI;
 
 use royal\Pioche;
 use royal\Spawner;
@@ -18,8 +21,52 @@ class Main extends PluginBase implements Listener{
     {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getLogger()->info( "§aPlugin Chargé avec succès !");
-        ItemFactory::registerItem(new Pioche(), true );
+        ItemFactory::getPluginManager(new Pioche(), true );
         $this->getPluginManager()->registerEvents(new Spawner($this), $this);
     }
+    public function onCommand(CommandSender $player, Command $command, string $label, array $args): bool
+    {
+        switch($command->getName()){
+            case "PiocheUI":
+                if($player instanceof Player){
+                    $this->openServerForm($player);
+                }
+            break;
+        }
+        return true;
+    }
+    public function openServerForm($player)
+    {
+        $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+        $form = $api->createSimpleForm(function (Player $player, int $data = null){
+            $result = $data;
+            if($result === null){
+                return true;
+            }
+            switch($result){
+                case 0:
+                    $myMoney = EconomyAPI::getInstance()->myMoney($player);
+
+                    $player->sendMessage("Vous avez : " . $myMoney . " en monnaie.");
+                break;
+
+                case 2:
+                    EconomyAPI::getInstance()->reduceMoney($player, 25);
+
+                    $myMoney = EconomyAPI::getInstance()->myMoney($player);
+
+
+                    $player->sendMessage("il ne vous reste que : " . $myMoney . " en monnaie.");
+                break;
+            }
+        });
+        $form->setTitle("PiocheSpawner");
+        $form->setContent("Acheter une pioche Spéciale spawner?");
+        $form->addButton("que me reste t'il ", $maMoney);
+        $form->addButton("Acheter la pioche ^^");
+        $form->sendToPlayer($player);
+        return $form;
+    }
 }
+
 
